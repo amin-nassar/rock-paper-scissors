60e8baa97fc2abb2f1eca70e90edf349f918ab31
amin-nassar <amin.nassar.ce@gmail.com>
2025-06-28 02:21:53 +0300

    Added sound effects to game events.

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 const GAME_MODES = { ORIGINAL: "ORIGINAL", BONUS: "BONUS" };
 
+const GAME_RESULTS = { WIN: "WIN", LOSE: "LOSE", DRAW: "DRAW" };
+
+const GAME_EVENTS = {
+  ...GAME_RESULTS,
+  USER_CHOICE: "USER_CHOICE",
+  HOUSE_CHOICE: "HOUSE_CHOICE",
+};
+
+const TEXT_BY_RESULT = {
+  [GAME_RESULTS.WIN]: "You Win",
+  [GAME_RESULTS.LOSE]: "You Lose",
+  [GAME_RESULTS.DRAW]: "Draw",
+};
+
 const ORIGINAL_RULES = {
   rock: ["scissors"],
   paper: ["rock"],
@@ -26,6 +40,16 @@ const RULES_IMG_PATH_BY_MODE = {
 
 const STORAGE_KEYS = { SCORE: "rps_score", MODE: "rps_mode" };
 
+const SOUND_PATHS = {
+  [GAME_EVENTS.DRAW]: "./sfx/draw.mp3",
+  [GAME_EVENTS.WIN]: "./sfx/win.mp3",
+  [GAME_EVENTS.LOSE]: "./sfx/lose.mp3",
+  [GAME_EVENTS.USER_CHOICE]: "./sfx/user-choice.mp3",
+  [GAME_EVENTS.HOUSE_CHOICE]: "./sfx/house-choice.wav",
+};
+
+const SOUNDS = {};
+
 const screens = {
   choose: document.querySelector(".choose"),
   waiting: document.querySelector(".waiting"),
@@ -50,6 +74,8 @@ function initializeGame() {
   scoreElement.textContent = score;
 
   attachEventListeners();
+  initializeAudio();
+
   setMode(mode);
   resetGame();
 }
@@ -63,6 +89,7 @@ function attachEventListeners() {
   screens.choose.addEventListener("click", (e) => {
     const userChoice = e.target.dataset.choice;
     if (!(userChoice in rules)) return;
+    playSound(GAME_EVENTS.USER_CHOICE);
     selectUserChoice(userChoice);
   });
 }
@@ -103,9 +130,10 @@ function getHouseChoice() {
   return options[Math.floor(Math.random() * options.length)];
 }
 
-function determineWinner(player, computer) {
-  if (player === computer) return "tie";
-  return rules[player]?.includes(computer) ? "win" : "lose";
+function determineWinner(userChoice, houseChoice) {
+  if (userChoice === houseChoice) return GAME_RESULTS.DRAW;
+  const userBeatsHouse = rules[userChoice].includes(houseChoice);
+  return userBeatsHouse ? GAME_RESULTS.WIN : GAME_RESULTS.LOSE;
 }
 
 function selectUserChoice(userChoice) {
@@ -115,22 +143,19 @@ function selectUserChoice(userChoice) {
   userChoiceBtn.setAttribute("data-choice", userChoice);
   switchScreen("waiting");
 
+  setTimeout(() => playSound(GAME_EVENTS.HOUSE_CHOICE), 500);
+
   const houseChoice = getHouseChoice();
+  const [userBtn, houseBtn] = screens.reveal.querySelectorAll(".choice-btn");
+  userBtn.setAttribute("data-choice", userChoice);
+  houseBtn.setAttribute("data-choice", houseChoice);
+  const result = determineWinner(userChoice, houseChoice);
+
+  document.getElementById("result-text").textContent = TEXT_BY_RESULT[result];
 
   setTimeout(() => {
-    const [userBtn, houseBtn] = screens.reveal.querySelectorAll(".choice-btn");
-    userBtn.setAttribute("data-choice", userChoice);
-    houseBtn.setAttribute("data-choice", houseChoice);
-
-    const result = determineWinner(userChoice, houseChoice);
-    document.getElementById("result-text").textContent = {
-      win: "You Win",
-      lose: "You Lose",
-      tie: "Draw",
-    }[result];
-
-    if (result === "win") score++;
-    if (result === "lose") score--;
+    if (result === GAME_RESULTS.WIN) score++;
+    if (result === GAME_RESULTS.LOSE) score--;
     score = Math.max(score, 0);
 
     localStorage.setItem(STORAGE_KEYS.SCORE, score);
@@ -139,10 +164,24 @@ function selectUserChoice(userChoice) {
     switchScreen("reveal");
     modeButton.style.visibility = "visible";
 
-    if (result === "tie") return;
-    const winnerBtn = result === "win" ? userBtn : houseBtn;
+    playSound(result);
+
+    if (result === GAME_RESULTS.DRAW) return;
+    const winnerBtn = result === GAME_RESULTS.WIN ? userBtn : houseBtn;
     winnerBtn.classList.add("winner");
-  }, 2000);
+  }, 3000);
+}
+
+function initializeAudio() {
+  for (const soundKey in SOUND_PATHS) {
+    const audioElement = new Audio(SOUND_PATHS[soundKey]);
+    // audioElement.load();
+    SOUNDS[soundKey] = audioElement;
+  }
+}
+
+function playSound(soundKey) {
+  SOUNDS[soundKey].play();
 }
 
 initializeGame();
